1c46c46a505482551f7b02d09812cbbcb061e441
Ming <minghaochen912@gmail.com>
2020-10-31 20:58:05 -0700

    works

--- main.ts
+++ main.ts
@@ -1,5 +1,13 @@
-const OpenCC = require('opencc');
+// const OpenCC = require('opencc');
+import pkg from 'opencc';
+const {OpenCC} = pkg;
+
+const s2t = new OpenCC('s2t.json');
+const t2s = new OpenCC('t2s.json');
 
 async function main() { 
-    console.log("a");
+    const result: string = await s2t.convertPromise("汉字");
+    console.log(result);
 }
+
+main();
