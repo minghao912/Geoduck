ae900f527483774a9de77019bffcd8f715967e4b
Ming <minghaochen912@gmail.com>
2020-12-08 11:58:37 -0800

    test response data

---
+++ abc.js
@@ -0,0 +1,18 @@
+const http = require('http');
+
+http.get('http://localhost:8080/test', (resp) => {
+  let data = '';
+
+  // A chunk of data has been recieved.
+  resp.on('data', (chunk) => {
+    data += chunk;
+  });
+
+  // The whole response has been received. Print out the result.
+  resp.on('end', () => {
+    console.log(data);
+  });
+
+}).on("error", (err) => {
+  console.log("Error: " + err.message);
+});
