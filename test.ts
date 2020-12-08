dae3dd0c7f30ac251afafd552a33204913afc5bb
Ming <minghaochen912@gmail.com>
2020-12-08 08:50:12 -0800

    switch to typescript

---
+++ test.ts
@@ -0,0 +1,9 @@
+import * as http from 'http';
+
+function onRequest(request, response) {
+    response.writeHead(200, {'Content-Type': 'text/plain'});
+    response.write("Hello World");
+    response.end();
+}
+
+http.createServer(onRequest).listen(8080);
