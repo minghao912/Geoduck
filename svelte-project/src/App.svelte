50bb2c124765800eab97176b5909e82abc6c06b5
Ming <minghaochen912@gmail.com>
2020-12-16 21:14:19 -0800

    get started with svelte

---
+++ svelte-project/src/App.svelte
@@ -0,0 +1,42 @@
+<script lang="ts">
+	import Input from "./Input.svelte";
+
+	export let name: string;
+	export let yeet: number;
+
+	let testVal = 888;
+	let testValStr = testVal as string;
+</script>
+
+<main>
+	<h1>Hello {name}!</h1>
+	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	
+	<br />
+
+	<p>This is a test: {yeet}</p>
+	<Input />
+	<Input value={testValStr} />
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
---
+++ svelte-project/src/Input.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+    export let value = "default";
+</script>
+
+<main>
+    <p>This is a test. Test value is {value}</p>
+</main>
+
+<style>
+    p {
+        color: purple;
+    }
+</style>
